eff674b82381f6eb31e96294a4d36c9d2d2fea2f
HCarrr <134705695+HCarrr@users.noreply.github.com>
2024-12-23 11:46:25 +0700

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "org.chevalier.composablecolumn"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
